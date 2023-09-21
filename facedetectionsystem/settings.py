ea323fc3cc7bcd40e17fefdae466a833999fbd9a
Zeeshan Saeed <zeeshansheikh0313@gmail.com>
2023-09-22 03:14:06 +0500

    add email information

--- facedetectionsystem/settings.py
+++ facedetectionsystem/settings.py
@@ -142,3 +142,10 @@ django_on_heroku.settings(locals())
 MESSAGE_TAGS={
 messages.ERROR: "danger"
 }
+#Email Information:
+EMAIL_HOST=os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER =os.environ.get("EMAIL_HOST_USER")
+EMAIL_USE_TLS= True
+DEFAULT_FROM_EMAIL= os.environ.get("EMAIL_HOST_USER")
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
